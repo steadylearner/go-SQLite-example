ce2b24573defffe104ff68e225b1773fc25543c2
luisf <stdiopt@gmail.com>
2020-03-22 11:53:07 -0300

    Add users as a repo, updated some sql stuff

--- models/users.go
+++ models/users.go
@@ -3,7 +3,8 @@ package models
 // Refer to this.
 // https://github.com/steadylearner/Rust-Full-Stack/blob/master/bots/teloxide/src/community-bots/models/subreddit.rs
 
+// User user thingy
 type User struct {
-	Id       int
-	Username string
+	ID   int64
+	Name string
 }
